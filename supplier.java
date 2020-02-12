b7a830a6d748ccb7606218fe882fc686f0797e17
Ixarias <lacareo@gmail.com>
2020-02-12 17:31:39 -0600

    Supplier Object Work

--- supplier.java
+++ supplier.java
@@ -1,9 +1,28 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 
 public class Supplier implements Serializable {
     private static final long serialVersionUID = 1L;
     private String name;
+    private String address;
+    private List<Product> catalogue = new LinkedList<Product>();
+
+    public Supplier (String name, String address, List<Product> catalogue) {
+        this.name = name;
+        this.address = address;
+        this.catalogue = catalogue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public List<Product> getCatalogue() {
+        return catalogue;
+    }
 
 }
