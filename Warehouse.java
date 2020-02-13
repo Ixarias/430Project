f3c412d6075e2e6f700e5c49cfa42d50c5a31e64
robbe <57543676+robbentenmann@users.noreply.github.com>
2020-02-12 20:30:41 -0800

    Update Warehouse.java

--- Warehouse.java
+++ Warehouse.java
@@ -1,32 +1,35 @@
 import java.util.*;
 import java.io.*;
-
 public class Warehouse implements Serializable {
   private static final long serialVersionUID = 1L;
 
-  // private Catalog catalog;
+  //private Catalog catalog;
+  private ProductList productList;  
   private ClientList clientList;
   private static Warehouse warehouse;
-
   private Warehouse() {
-    // catalog = Catalog.instance();
+    //catalog = Catalog.instance();
     clientList = ClientList.instance();
+    productList = ProductList.instance();
   }
-
   public static Warehouse instance() {
     if (warehouse == null) {
-      // MemberIdServer.instance(); // instantiate all singletons
+      //MemberIdServer.instance(); // instantiate all singletons
       return (warehouse = new Warehouse());
     } else {
       return warehouse;
     }
   }
-
+  
   /*
-   * public Book addBook(String title, String author, String id) { Book book = new
-   * Book(title, author, id); if (catalog.insertBook(book)) { return (book); }
-   * return null; }
-   */
+  public Book addBook(String title, String author, String id) {
+    Book book = new Book(title, author, id);
+    if (catalog.insertBook(book)) {
+      return (book);
+    }
+    return null;
+  }
+  */
   public Client addClient(String name, String address, String phone, String id) {
     Client client = new Client(name, address, phone, id);
     if (clientList.insertClient(client)) {
@@ -35,52 +38,63 @@ public class Warehouse implements Serializable {
     return null;
   }
 
-  /*
-   * public Iterator getBooks() { return catalog.getBooks(); }
-   */
+  public Product addProduct(String name, String supplier, double price) {
+    Product product = new Product(name, supplier, price);
+    if (productList.insertProduct(product)) {
+      return (product);
+    }
+    return null;
+  }
+
+/*
+  public Iterator getBooks() {
+      return catalog.getBooks();
+  }
+*/
 
   public Iterator getClients() {
-    return clientList.getClients();
+      return clientList.getClients();
   }
+
+  public Iterator getProducts() {
+    return productList.getProducts();
+}
 
   public static Warehouse retrieve() {
     try {
       FileInputStream file = new FileInputStream("WarehouseData");
       ObjectInputStream input = new ObjectInputStream(file);
       input.readObject();
-      // ClientIdServer.retrieve(input);
+      //ClientIdServer.retrieve(input);
       return warehouse;
-    } catch (IOException ioe) {
+    } catch(IOException ioe) {
       ioe.printStackTrace();
       return null;
-    } catch (ClassNotFoundException cnfe) {
+    } catch(ClassNotFoundException cnfe) {
       cnfe.printStackTrace();
       return null;
     }
   }
-
-  public static boolean save() {
+  public static  boolean save() {
     try {
       FileOutputStream file = new FileOutputStream("WarehouseData");
       ObjectOutputStream output = new ObjectOutputStream(file);
       output.writeObject(warehouse);
-      // output.writeObject(ClientIdServer.instance());
+      //output.writeObject(ClientIdServer.instance());
       return true;
-    } catch (IOException ioe) {
+    } catch(IOException ioe) {
       ioe.printStackTrace();
       return false;
     }
   }
-
   private void writeObject(java.io.ObjectOutputStream output) {
     try {
       output.defaultWriteObject();
       output.writeObject(warehouse);
-    } catch (IOException ioe) {
+    } catch(IOException ioe) {
       System.out.println(ioe);
     }
   }
-
   private void readObject(java.io.ObjectInputStream input) {
     try {
       input.defaultReadObject();
@@ -89,14 +103,14 @@ public class Warehouse implements Serializable {
       } else {
         input.readObject();
       }
-    } catch (IOException ioe) {
+    } catch(IOException ioe) {
       ioe.printStackTrace();
-    } catch (Exception e) {
+    } catch(Exception e) {
       e.printStackTrace();
     }
   }
-
   public String toString() {
-    return /* catalog + "\n" + */ clientList.toString();
+    return /*catalog + "\n" + */ clientList.toString();
+    return productList.toString();
   }
 }
