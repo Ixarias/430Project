8c24e7eaef1834a036f44378b5c5cde032acc7d7
robbe <57543676+robbentenmann@users.noreply.github.com>
2020-02-20 23:40:53 -0800

    invoice and server

--- Invoice.java
+++ Invoice.java
@@ -1,12 +1,33 @@
 import java.util.*;
 import java.io.*;
 
-public class Invoice {
-    String id;
+public class Invoice implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private String id;
+  private int grandTotal;
+  private static final String CLIENT_STRING = "I";
 
-    Invoice() {
-        // Constructor
-    }
+  public Invoice(String id, int grandTotal{
+    this.id = INVOICE_STRING + (InvoiceIdServer.instance()).getId();
+    this.grandTotal;
+  }
 
+  public String getId() {
+    return id;
+  }
 
+  public int getTotal() {
+    return grandTotal;
+  }
+
+  public void setTotal(int newTotal) {
+    grandTotal = newTotal;
+
+  public boolean equals(String id) {
+    return this.id.equals(id);
+  }
+
+  public String toString() {
+    return "Invoice id: " + id + " | Grand Total = " + grandTotal;
+  }
 }
---
+++ InvoiceIdServer.java
@@ -0,0 +1,50 @@
+import java.io.*;
+public class InvoiceIdServer implements Serializable {
+  private  int idCounter;
+  private static InvoiceIdServer server;
+  private InvoiceIdServer() {
+    idCounter = 1;
+  }
+  public static InvoiceIdServer instance() {
+    if (server == null) {
+      return (server = new InvoiceIdServer ());
+    } else {
+      return server;
+    }
+  }
+  public int getId() {
+    return idCounter++; // Return current count, then increment
+  }
+  public String toString() {
+    return ("IdServer" + idCounter);
+  }
+  public static void retrieve(ObjectInputStream input) {
+    try {
+      server = (InvoiceIdServer) input.readObject();
+    } catch(IOException ioe) {
+      ioe.printStackTrace();
+    } catch(Exception cnfe) {
+      cnfe.printStackTrace();
+    }
+  }
+  private void writeObject(java.io.ObjectOutputStream output) throws IOException {
+    try {
+      output.defaultWriteObject();
+      output.writeObject(server);
+    } catch(IOException ioe) {
+      ioe.printStackTrace();
+    }
+  }
+  private void readObject(java.io.ObjectInputStream input) throws IOException, ClassNotFoundException {
+    try {
+      input.defaultReadObject();
+      if (server == null) {
+        server = (ClientIdServer) input.readObject();
+      } else {
+        input.readObject();
+      }
+    } catch(IOException ioe) {
+      ioe.printStackTrace();
+    }
+  }
+}
