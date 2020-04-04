b4e3270861c7c575d058c3f889839aac07313930
robbe <57543676+robbentenmann@users.noreply.github.com>
2020-04-03 21:38:29 -0500

    add a function
    added the function show suppliers of product

--- Warehouse.java
+++ Warehouse.java
@@ -228,6 +228,23 @@ public class Warehouse implements Serializable {
     return false;
   }
 
+  public boolean showSuppliersOfProduct(String targetName) {
+    Iterator<Product> allProducts = getProducts();
+    // search for product by name
+    // (iterate until product.name == name)
+    while (allProducts.hasNext()) {
+      Product product = (Product) (allProducts.next());
+      // String name = product.getName();
+      // System.out.println(name);
+      if (product.equals(targetName)) {
+        //System.out.println("Name " + targetName + " found.");
+        product.getCatalog();
+        return true;
+      }
+    }
+    return false;
+  }
+  
   public boolean showProductsOfSupplier(String targetName) {
     Iterator<Supplier> allSuppliers = getSuppliers();
     // search for supplier by name
